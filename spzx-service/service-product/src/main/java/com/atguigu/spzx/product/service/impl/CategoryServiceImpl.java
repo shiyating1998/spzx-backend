b546d06e329e1e81229b71f6bc6ca6436283f9da
Jennifer Shi <shiyating1998@gmail.com>
2024-02-29 21:41:47 -0500

    feb 29 P83

--- spzx-service/service-product/src/main/java/com/atguigu/spzx/product/service/impl/CategoryServiceImpl.java
+++ spzx-service/service-product/src/main/java/com/atguigu/spzx/product/service/impl/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package com.atguigu.spzx.product.service.impl;
 import com.atguigu.spzx.model.entity.product.Category;
 import com.atguigu.spzx.product.mapper.CategoryMapper;
 import com.atguigu.spzx.product.service.CategoryService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -16,5 +18,27 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<Category> findOneCategory() {
         return categoryMapper.findOneCategory();
+    }
+
+    @Override
+    public List<Category> findCategoryTree() {
+        List<Category> categoryList = categoryMapper.findAll();
+        //全部一级分类
+        List<Category> oneCategoryList = categoryList.stream().filter(item -> item.getParentId().longValue() == 0).collect(Collectors.toList());
+
+        if(!CollectionUtils.isEmpty(oneCategoryList)) {
+            oneCategoryList.forEach(oneCategory -> {
+                List<Category> twoCategoryList = categoryList.stream().filter(item -> item.getParentId().longValue() == oneCategory.getId().longValue()).collect(Collectors.toList());
+                oneCategory.setChildren(twoCategoryList);
+
+                if(!CollectionUtils.isEmpty(twoCategoryList)) {
+                    twoCategoryList.forEach(twoCategory -> {
+                        List<Category> threeCategoryList = categoryList.stream().filter(item -> item.getParentId().longValue() == twoCategory.getId().longValue()).collect(Collectors.toList());
+                        twoCategory.setChildren(threeCategoryList);
+                    });
+                }
+            });
+        }
+        return oneCategoryList;
     }
 }
