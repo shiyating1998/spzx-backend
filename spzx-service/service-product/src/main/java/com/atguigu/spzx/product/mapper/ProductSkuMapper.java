00d7a63e685f4c2c97cace9bdaed88eec909ad56
Jennifer Shi <shiyating1998@gmail.com>
2024-03-06 19:18:45 -0500

    p89

--- spzx-service/service-product/src/main/java/com/atguigu/spzx/product/mapper/ProductSkuMapper.java
+++ spzx-service/service-product/src/main/java/com/atguigu/spzx/product/mapper/ProductSkuMapper.java
@@ -11,4 +11,8 @@ public interface ProductSkuMapper {
     List<ProductSku> findProductSkuBySale();
 
     List<ProductSku> findByPage(ProductSkuDto productSkuDto);
+
+    List<ProductSku> findByProductId(Long productId);
+
+    ProductSku getById(Long skuId);
 }
